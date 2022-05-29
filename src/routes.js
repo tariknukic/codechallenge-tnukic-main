cd0d5c90128273e2c4d8b3b289ba830ea45b3f52
Tarik Nukic <tnukm@outlook.com>
2022-05-29 16:43:57 +0200

    Update routes.js

--- src/routes.js
+++ src/routes.js
@@ -84,7 +84,8 @@ router.post('/assigned', asyncHanlder(async (req, res, next)=>{
         const teamId = await crud.findTeamForProject(projectId);
 
         if (teamId) {
-            res.json({"ID": teamId});
+            res.set({'Content-Type': 'text/javascript'});
+            res.json(`ID=${teamId}`);
         } else {
             const projectIsWaiting = await crud.findWaitingProject(projectId);
 
